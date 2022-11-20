4323d4aceffb9d3c4835a99d2739c9db693f2d21
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-11-20 18:25:09 +0100

    url

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 from selenium import webdriver
- 
 os.environ['PATH'] += r"C:\Users\khali\Desktop\projects\Python_Automation\chromedriver_win32"
 driver = webdriver.Chrome()
+
+
+url = "https://www.x-kom.pl/"
+driver.get(url)
